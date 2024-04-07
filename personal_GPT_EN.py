6c4967035b9fd87dc7233a3f5501248866e37e46
Matheus Passos <40068795+profmatheuspassos@users.noreply.github.com>
2024-04-07 19:59:20 +0300

    v. 1.0

--- personal_GPT_EN.py
+++ personal_GPT_EN.py
@@ -1,7 +1,6 @@
 # Version 1.0
 # Date: April 7, 2024
-# DISCLAIMER
-# This script is provided "as is", without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the script or the use or other dealings in the script.
+# DISCLAIMER: This script is provided "as is", without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the script or the use or other dealings in the script.
 
 import os
 import openai
